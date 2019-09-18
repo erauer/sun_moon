6df795c4d2ad3d5eb016be44e76cf45b95500bce
Eric Rauer <erauer@users.noreply.github.com>
2019-09-18 10:27:05 -0700

    Upgrade to Elixir 1.9 (#1)
    * Update to Nerves 1.5
    
    * Switch to separate weekday and weekend schedules

--- lib/sun_moon/routine.ex
+++ lib/sun_moon/routine.ex
@@ -4,14 +4,16 @@ defmodule SunMoon.Routine do
   @sun {128, 64, 0}
   @moon {0, 0, 64}
   @off {0, 0, 0}
+  @red {64, 0, 0}
 
   require Logger
 
   def moon(), do: render(@moon)
   def sun(), do: render(@sun)
   def off(), do: render(@off)
+  def red(), do: render(@red)
 
-  defp render(color) do
+  def render(color) do
     Blinkchain.fill({0, 0}, 8, 4, color)
     Blinkchain.render()
   end
